8f05e595ff62d4ae64c208e219dc2e3efabecae7
Joren Verbesselt <jorenverbesselt@gmail.com>
2022-11-03 09:24:05 +0100

    testing with tests

--- test/productForm.test.ts
+++ test/productForm.test.ts
@@ -7,6 +7,7 @@ import {
   within,
 } from '@testing-library/vue'
 import userEvent from '@testing-library/user-event'
+// import flushPromises from 'flush-promises'
 
 import { createI18n } from 'vue-i18n'
 
@@ -28,41 +29,49 @@ describe('ProductForm', () => {
 
     // submit form
     const button = screen.getByRole('button')
-    // fireEvent.click(button)
+    await fireEvent.click(button)
+    // await flushPromises()
 
     // expect title field to have error
-    // const errors = screen.getByRole('error', { name: 'String must contain at least 1 character(s)' })
+    const errors = screen.getByRole('error')
+    waitFor(() => {
+      expect(errors.innerHTML).toContain('blabla')
+    })
 
     // get input, and type 'Product 1'
     const title = screen.getByLabelText('product.title')
-    await userEvent.type(title, 'Product 1')
+    // await userEvent.type(title, 'Product 1')
+    await fireEvent.update(title, 'Product 1')
 
     // get input, and type
     const sku = screen.getByLabelText('product.sku')
-    await userEvent.type(sku, 'sku-product-1')
+    // await userEvent.type(sku, 'sku-product-1')
+    await fireEvent.update(sku, 'sku-product-1')
 
     // get input, and type
     const price = screen.getByLabelText('product.price')
-    await userEvent.type(price, '55')
+    // await userEvent.type(price, '55')
+    await fireEvent.update(price, '55')
 
     // get input, and type
     const basePrice = screen.getByLabelText('product.basePrice')
-    await userEvent.type(basePrice, '44')
+    // await userEvent.type(price, '44')
+    await fireEvent.update(basePrice, '44')
 
     // get checkbox and click
     const stocked = screen.getByLabelText('product.stocked')
     fireEvent.click(stocked)
 
     // submit form
-    fireEvent.click(button)
+    // fireEvent.click(button)
 
-    const submitArg = emitted<any>().submit[0][0]
-    expect(submitArg).toEqual({
-      title: 'Product 1',
-      sku: 'sku-product-1',
-      price: 55,
-      basePrice: 44,
-      stocked: true,
-    })
+    // const submitArg = emitted<any>().submit[0][0]
+    // expect(submitArg).toEqual({
+    //   title: 'Product 1',
+    //   sku: 'sku-product-1',
+    //   price: 55,
+    //   basePrice: 44,
+    //   stocked: true,
+    // })
   })
 })
