40dd59d71c107a9e3c2deec5225c63604715c21d
Joren Verbesselt <jorenverbesselt@gmail.com>
2022-11-07 15:04:56 +0100

    mock basket function

--- test/basket.test.ts
+++ test/basket.test.ts
@@ -1,5 +1,6 @@
-import { beforeEach, describe, expect, it } from 'vitest'
+import { beforeEach, describe, expect, it, vi } from 'vitest'
 import { createPinia, setActivePinia } from 'pinia'
+import * as basketApi from '../src/api/basket'
 import { useBasketStore } from '~/store/basket'
 
 describe('basket', () => {
@@ -9,27 +10,27 @@ describe('basket', () => {
     // `useStore(pinia)`
     setActivePinia(createPinia())
   })
-  it('products total and amount functions', () => {
+  it('products total and amount functions', async () => {
+    vi.spyOn(basketApi, 'addProductToBasket').mockResolvedValue([{
+      id: 1,
+      productId: 3,
+      quantity: 4,
+      product: {
+        id: 3,
+        title: 'Product 1',
+        sku: 'sku-product-1',
+        price: 5.1,
+        basePrice: 4,
+        stocked: false,
+        image: '/images/products/test.png',
+      },
+    }])
+
     const basket = useBasketStore()
     expect(basket.numberOfProducts).toBe(0)
     expect(basket.total).toBe('0.00')
 
-    basket.items = [
-      {
-        id: 1,
-        productId: 3,
-        quantity: 4,
-        product: {
-          id: 3,
-          title: 'Product 1',
-          sku: 'sku-product-1',
-          price: 5.1,
-          basePrice: 4,
-          stocked: false,
-          image: '/images/products/test.png',
-        },
-      },
-    ]
+    await basket.add(3)
 
     expect(basket.numberOfProducts).toBe(1)
     expect(basket.total).toBe('20.40')
